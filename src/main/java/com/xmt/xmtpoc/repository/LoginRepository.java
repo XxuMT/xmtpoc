7db51c1f987dcfb92e6814b9980f5e83ebd41818
mengting.xu <mengting.xu@ucarinc.com>
2019-04-30 14:44:13 +0800

    type: feature: jpa使用@Query注解写sql，修改密码

--- src/main/java/com/xmt/xmtpoc/repository/LoginRepository.java
+++ src/main/java/com/xmt/xmtpoc/repository/LoginRepository.java
@@ -2,6 +2,9 @@ package com.xmt.xmtpoc.repository;
 
 import com.xmt.xmtpoc.dto.domain.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author mengting.xu
@@ -22,4 +25,22 @@ public interface LoginRepository extends JpaRepository<UserInfo, Long> {
      * @return
      */
     UserInfo findByUserNameAndUserPass(String userName, String userPass);
+
+    /**
+     *
+     * description: 根据Id修改密码
+     *
+     * 需要加上注解 @Transactional 开启事务，不然报错
+     * javax.persistence.TransactionRequiredException: Executing an update/delete query
+     *
+     * @author mengting.xu
+     * @date 2019/4/30 11:07
+     * @param id
+     * @param userPass
+     * @return
+     */
+    @Modifying
+    @Transactional(rollbackFor = Exception.class, timeout = 10)
+    @Query("update UserInfo user set user.userPass = ?1 where user.id = ?2")
+    int modifyUserPassById(String userPass, Long id);
 }
